ea1b92f435d7dbe3c89b6f1ba15f10fdf86523aa
Gloria Friesen and Megan Warnock <megan@>
2017-03-29 11:07:35 -0700

    pass DogPark class tests

--- src/main/java/DogPark.java
+++ src/main/java/DogPark.java
@@ -1,16 +1,82 @@
 import org.sql2o.*;
+import java.util.List;
 
 public class DogPark {
   private String name;
   private String address;
   private String notes;
-  private int locationId;
+  private int location_id;
+  private int id;
 
-  public DogPark(String name, String address, String notes, int locationId) {
+  public DogPark(String name, String address, String notes, int location_id) {
     this.name = name;
     this.address = address;
     this.notes = notes;
-    this.locationId = locationId;
+    this.location_id = location_id;
   }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+
+  public int getLocationId() {
+    return location_id;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public static List<DogPark> all() {
+    String sql = "SELECT id, name, address, notes, location_id FROM parks";
+    try (Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(DogPark.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherDogPark){
+    if (!(otherDogPark instanceof DogPark)) {
+      return false;
+    } else {
+      DogPark newDogPark = (DogPark) otherDogPark;
+      return
+      this.getName().equals(newDogPark.getName()) &&     this.getAddress().equals(newDogPark.getAddress()) && this.getNotes().equals(newDogPark.getNotes()) &&
+      this.getLocationId() == newDogPark.getLocationId() &&
+      this.getId() == newDogPark.getId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO parks (name, address, notes, location_id) VALUES (:name, :address, :notes, :location_id)";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", this.name)
+      .addParameter("address", this.address)
+      .addParameter("notes", this.notes)
+      .addParameter("location_id", this.location_id)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+  public static DogPark find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM parks WHERE id=:id";
+      DogPark dogPark = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(DogPark.class);
+      return dogPark;
+    }
+  }
+
 
 }
--- src/test/java/DogParkTest.java
+++ src/test/java/DogParkTest.java
@@ -1,6 +1,7 @@
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.List;
 
 public class DogParkTest {
 
@@ -23,4 +24,69 @@ public class DogParkTest {
     assertEquals(true, newDogPark instanceof DogPark);
   }
 
+  @Test
+  public void getName_getsNameValue_name() {
+    DogPark newDogPark = new DogPark("Wallace", "1600 NW 25th Ave", "cool park", 1);
+    assertEquals("Wallace", newDogPark.getName());
+  }
+
+  @Test
+  public void getAddress_getsAddressValue_address() {
+    DogPark newDogPark = new DogPark("Wallace", "1600 NW 25th Ave", "cool park", 1);
+    assertEquals("1600 NW 25th Ave", newDogPark.getAddress());
+  }
+
+  @Test
+  public void getNotes_getsNotesValue_notes() {
+    DogPark newDogPark = new DogPark("Wallace", "1600 NW 25th Ave", "cool park", 1);
+    assertEquals("cool park", newDogPark.getNotes());
+  }
+
+  @Test
+  public void getId_returnsInstanceOfId_1() {
+    DogPark newDogPark = new DogPark("Wallace", "1600 NW 25th Ave", "cool park", 1);
+    newDogPark.save();
+    assertTrue(newDogPark.getId() > 0);
+  }
+
+  @Test
+  public void all_getsAllDogParks_true() {
+    DogPark firstDogPark = new DogPark("Wallace", "1600 NW 25th Ave", "cool park", 1);
+    firstDogPark.save();
+    DogPark secondDogPark = new DogPark("Overlook", "1599 N Fremont St", "best park", 2);
+    secondDogPark.save();
+    assertEquals(true, DogPark.all().get(0).equals(firstDogPark));
+    assertEquals(true, DogPark.all().get(1).equals(secondDogPark));
+  }
+
+  @Test
+  public void equals_returnsTrueifNameIsTheSame() {
+    DogPark firstDogPark = new DogPark("Wallace", "1600 NW 25th Ave", "cool park", 1);
+    DogPark secondDogPark = new DogPark("Wallace", "1600 NW 25th Ave", "cool park", 1);
+    assertTrue(firstDogPark.equals(secondDogPark));
+  }
+
+  @Test
+  public void save_savesIntoDatabase_true() {
+    DogPark newDogPark = new DogPark("Wallace", "1600 NW 25th Ave", "cool park", 1);
+    newDogPark.save();
+    assertTrue(DogPark.all().get(0).equals(newDogPark));
+  }
+
+  @Test
+  public void save_assignsIdToDogPark() {
+    DogPark newDogPark = new DogPark("Wallace", "1600 NW 25th Ave", "cool park", 1);
+    newDogPark.save();
+    DogPark savedDogPark = DogPark.all().get(0);
+    assertEquals(newDogPark.getId(), savedDogPark.getId());
+  }
+
+  @Test
+  public void find_returnsDogParkWithSameId_secondDogPark() {
+    DogPark firstDogPark = new DogPark("Wallace", "1600 NW 25th Ave", "cool park", 1);
+    firstDogPark.save();
+    DogPark secondDogPark = new DogPark("Overlook", "1599 N Fremont St", "best park", 2);
+    secondDogPark.save();
+    assertEquals(DogPark.find(secondDogPark.getId()), secondDogPark);
+  }
 }
