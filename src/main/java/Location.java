ee744fbe587d23241aed3241bc42f0728c62d420
Gloria Friesen and Megan Warnock <gloria@>
2017-03-29 10:24:37 -0700

    pass tests for Location class

--- src/main/java/Location.java
+++ src/main/java/Location.java
@@ -1,7 +1,9 @@
 import org.sql2o.*;
+import java.util.List;
 
 public class Location {
 private String neighborhood;
+private int id;
 
   public Location(String neighborhood) {
     this.neighborhood = neighborhood;
@@ -10,5 +12,48 @@ private String neighborhood;
   public String getNeighborhood() {
     return neighborhood;
   }
+
+  public int getId() {
+    return id;
+  }
+
+  public static List<Location> all() {
+    String sql = "SELECT id, neighborhood FROM locations";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Location.class);
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherLocation){
+    if (!(otherLocation instanceof Location)) {
+      return false;
+    } else {
+      Location newLocation = (Location) otherLocation;
+      return this.getNeighborhood().equals(newLocation.getNeighborhood()) && this.getId() == newLocation.getId();
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO locations (neighborhood) VALUES (:neighborhood)";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("neighborhood", this.neighborhood)
+      .executeUpdate()
+      .getKey();
+    }
+  }
+
+
+public static Location find(int id) {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "SELECT * FROM locations WHERE id = :id";
+    Location location = con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetchFirst(Location.class);
+    return location;
+  }
+}
+
 
 }
--- src/test/java/LocationTest.java
+++ src/test/java/LocationTest.java
@@ -1,6 +1,7 @@
 import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.List;
 
 public class LocationTest {
 
@@ -17,16 +18,66 @@ public class LocationTest {
     }
   }
 
-@Test
-public void Location_returnsInstanceOfLocation_true(){
-  Location newLocation = new Location("Pearl");
-  assertEquals(true, newLocation instanceof Location);
-}
+  @Test
+  public void Location_returnsInstanceOfLocation_true(){
+    Location newLocation = new Location("Pearl");
+    assertEquals(true, newLocation instanceof Location);
+  }
 
-@Test
-public void getNeighborhood_returnsInstanceOfNeighborhood_pearl(){
-  Location newLocation = new Location("Pearl");
-  assertEquals("Pearl", newLocation.getNeighborhood());
-}
+  @Test
+  public void getNeighborhood_returnsInstanceOfNeighborhood_pearl(){
+    Location newLocation = new Location("Pearl");
+    assertEquals("Pearl", newLocation.getNeighborhood());
+  }
+
+  @Test
+  public void getId_returnsInstanceOfId_1(){
+    Location newLocation = new Location("Pearl");
+    newLocation.save();
+    assertTrue(newLocation.getId() > 0);
+  }
+
+  @Test
+  public void all_getsAllLocations_true() {
+    Location firstLocation = new Location("Pearl");
+    firstLocation.save();
+    Location secondLocation = new Location("NW");
+    secondLocation.save();
+    assertEquals(true, Location.all().get(0).equals(firstLocation));
+    assertEquals(true, Location.all().get(1).equals(secondLocation));
+  }
+
+  @Test
+  public void equals_returnsTrueIfNeighborhoodIsTheSame() {
+    Location firstLocation = new Location("Pearl");
+    Location secondLocation = new Location("Pearl");
+    assertTrue(firstLocation.equals(secondLocation));
+  }
+
+  @Test
+  public void save_savesIntoDatabase_true() {
+    Location newLocation = new Location("Pearl");
+    newLocation.save();
+    assertTrue(Location.all().get(0).equals(newLocation));
+  }
+
+  @Test
+  public void save_assignsIdToLocation() {
+    Location newLocation = new Location("Pearl");
+    newLocation.save();
+    Location savedLocation = Location.all().get(0);
+    assertEquals(newLocation.getId(), savedLocation.getId());
+  }
+
+  @Test
+  public void find_returnsLocationWithSameId_secondLocation() {
+    Location firstLocation = new Location("Pearl");
+    firstLocation.save();
+    Location secondLocation = new Location("NW");
+    secondLocation.save();
+    assertEquals(Location.find(secondLocation.getId()), secondLocation);
+  }
+
+
 
 }
